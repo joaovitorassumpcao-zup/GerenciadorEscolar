0d1b79d2d43711874d9590a627b0d89862d3f42d
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-08-11 15:20:00 -0300

    Initial

---
+++ src/main/java/org/escola/gerenciador/GerenciadorEscolarApplication.java
@@ -0,0 +1,13 @@
+package org.escola.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorEscolarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorEscolarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/escola/gerenciador/GerenciadorEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.escola.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEscolarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
